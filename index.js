e9de213775a0f9a6743c07c1a5222128254bd0f7
pablitar <pablitar@gmail.com>
2019-03-31 16:31:55 -0300

    Issue #3: Agregada pepita funcional

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 export const pepita = {
+  nombre: "pepita",
   energia: 30,
-  vola: function(km) {
+  vola: function (km) {
     this.energia -= km * 2
   },
-  come: function(gramos) {
+  come: function (gramos) {
     this.energia += gramos * 3
   }
+}
+
+
+export const GolondrinasInmutables = {
+  create(nombre, edad = 1, energia = 30) {
+    return {
+      nombre,
+      edad,
+      energia
+    }
+  },
+
+  vola(golondrina, km) {
+    return {...golondrina, energia: golondrina.energia - km * 2}
+  },
+
+  come(golondrina, gramos) {
+    return {...golondrina, energia: golondrina.energia + gramos * 2}
+  }
+
 }
--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,15 @@
-import { pepita } from './index';
+import { pepita, GolondrinasInmutables as GI} from './index';
 
 describe("pepita basica", () => {
   it("deberia volar y perder energia", () => {
     pepita.vola(2)
     expect(pepita.energia).toEqual(26)
+  })
+})
+
+describe("pepita immutable", () => {
+  it("deberia volar y perder energia", () => {
+    const pepita = GI.vola(GI.create("pepita"), 2)
+    expect(pepita).toMatchObject({energia: 26, nombre: "pepita", "edad": 1})
   })
 })
